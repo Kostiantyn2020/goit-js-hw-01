871b72eb85dcc50006d9ed16c784753e2cf55fda
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2024-05-17 18:18:31 +0300

    3

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,15 @@
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
+function getShippingMessage(country, price, deliveryFee)
+{
 
-// Проверка корректности работы функции
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+   // Розрахунок загальної вартості замовлення
+  const totalPrice = price + deliveryFee;
+
+  // Повідомлення про доставку товару
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
+};
+
+// Перевірка работи функції
+//console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+//console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+//console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(getShippingMessage("Ukraine", 100, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -11,12 +11,13 @@ function getElementWidth(content, padding, border) {
   const borderWidth = Number.parseFloat(border);
 
   // Розраховуемо загальну ширину елемента
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  const borderBox = contentWidth + 2 * paddingWidth + 2 * borderWidth;
 
-  return totalWidth;
-}
+  return borderBox;
+};
 
-// Проверка корректности работы функции
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Перевірка работи функції
+//console.log(getElementWidth("50px", "8px", "4px")); // 74
+//console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+//console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("20,0px", "0.5px", "0,5px"));
